571de5cf2bdf7f84c02601c81a6e18a4ef94840d
violog <51th.apprent1ce.f0rce@gmail.com>
2023-02-02 19:32:08 +0200

    Fix uint256 validation

--- internal/service/requests/validation.go
+++ internal/service/requests/validation.go
@@ -1,10 +1,9 @@
 package requests
 
 import (
-	"math"
+	"math/big"
 	"regexp"
 	"strconv"
-	"strings"
 
 	"github.com/Swapica/order-aggregator-svc/resources"
 	val "github.com/go-ozzo/ozzo-validation/v4"
@@ -17,13 +16,12 @@ const (
 )
 
 var addressRegexp = regexp.MustCompile("^0x[0-9A-Fa-f]{40}$")
+var maxUint256 = new(big.Int).Exp(big.NewInt(2), big.NewInt(amountBitSize), nil)
+var bigZero = big.NewInt(0)
 
 func validateUint(value string, bitSize int) error {
 	if value == "" {
 		return val.ErrRequired
-	}
-	if strings.ContainsAny(value, "eE") {
-		return val.ErrMatchInvalid
 	}
 
 	if bitSize <= 64 {
@@ -31,25 +29,24 @@ func validateUint(value string, bitSize int) error {
 		return err
 	}
 
-	parsed, err := strconv.ParseFloat(value, 64)
-	if err != nil {
-		return err
+	parsed, success := new(big.Int).SetString(value, 10)
+	if !success {
+		return errors.Errorf("failed to parse big integer: string=%s", value)
 	}
-	if parsed < 0 || math.Mod(parsed, 1.0) != 0 {
-		return errors.Errorf("parsed value is not uint%d: parsed=%v", bitSize, parsed)
-	}
-	if max := math.Pow(2, float64(bitSize)); parsed > max {
-		return errors.Errorf("parsed value exceeds maximum: parsed=%v max=%v", parsed, max)
+	if isOutOfRange(parsed, bitSize) {
+		return errors.Errorf("parsed value is out of range for uint%d type: parsed=%s", bitSize, parsed.String())
 	}
 
 	return nil
 }
 
-func validateOptionalUint(value string, bitSize int) error {
-	if value == "" {
-		return nil
+func isOutOfRange(n *big.Int, bitSize int) bool {
+	if bitSize == amountBitSize {
+		return n.Cmp(bigZero) == -1 || n.Cmp(maxUint256) == 1
 	}
-	return validateUint(value, bitSize)
+
+	max := new(big.Int).Exp(big.NewInt(2), big.NewInt(int64(bitSize)), nil)
+	return n.Cmp(bigZero) == -1 || n.Cmp(max) == 1
 }
 
 func toDecodeErr(err error, what string) error {
