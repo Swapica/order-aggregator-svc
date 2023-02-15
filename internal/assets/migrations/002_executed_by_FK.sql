9b16f21a1a27e95430c5847afc7f8e0864f4d57d
violog <51th.apprent1ce.f0rce@gmail.com>
2023-02-15 16:08:58 +0200

    Fix migration

--- internal/assets/migrations/002_executed_by_FK.sql
+++ internal/assets/migrations/002_executed_by_FK.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE orders
-    ADD CONSTRAINT match_id_fk FOREIGN KEY (match_id, destination_chain) REFERENCES match_orders (match_id, src_chain);
+    ADD CONSTRAINT match_id_fk FOREIGN KEY (match_id, dest_chain) REFERENCES match_orders (match_id, src_chain);
 
 -- +migrate Down
 ALTER TABLE orders
